2eb8150a3c192ffb4ec30f96f1c6d526201510a8
Fatou leye <fatouleye412@gmail.com>
2026-09-20 14:50:47 +0000

    Add files via upload

---
+++ backend/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>TaskManager</title>
+</head>
+<body>
+
+    <h1>Bienvenue sur TaskManager</h1>
+
+    <p>Ma première page Laravel fonctionne !</p>
+
+</body>
+</html>
